09e1798973977d6c365d26a1f01fe0dd3e5698b8
Andrea Grandi <a.grandi@gmail.com>
2017-09-06 15:42:09 +0200

    Add Terraform options to bash script

--- databox.sh
+++ databox.sh
@@ -1,13 +1,49 @@
 #!/bin/sh
 
+while [[ $# -gt 1 ]]
+do
+key="$1"
+
+case $key in
+    -r|--region)
+    REGION="$2"
+    shift # past argument
+    ;;
+    -u|--username)
+    USERNAME="$2"
+    shift # past argument
+    ;;
+    -i|--instance)
+    INSTANCE="$2"
+    shift # past argument
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+shift # past argument or value
+done
+
 case "$1" in
 
 "up") echo "Creating DataBox"
-    # Get user from logged in username
-    export DATABOX_USER=`whoami`
+    # If I don't specify the region use a default value
+    if [ -z "${REGION+x}" ]; then
+        export REGION="eu-west-2"
+    fi
+
+    # If I don't specify the username get it from logged in user
+    if [ -z "${USERNAME+x}" ]; then
+        export USERNAME=`whoami`
+    fi
+
+    # If I don't specify the instance type use a default value
+    if [ -z "${INSTANCE+x}" ]; then
+        export INSTANCE="t2.micro"
+    fi
 
     # Launc Terraform passing that user as parameter
-    terraform apply --var username=$DATABOX_USER
+    terraform apply --var username=$USERNAME --var aws_region=$REGION --var instance_type=$INSTANCE
 
     # Get DataBox IP from state after the script completes
     export DATABOX_IP=`terraform output ec2_ip`
@@ -20,5 +56,9 @@ case "$1" in
     ;;
 *)  echo "DataBox - create and destroy AWS instances for Data Science"
     echo "./databox.sh up - Create a DataBox"
+    echo " -- options -- "
+    echo "  -r|--region - AWS region"
+    echo "  -u|--username - Username used to name the AWS objects"
+    echo "  -i|--instance - AWS instance type"
     echo "./databox.sh down - Destroy the DataBox"
 esac
